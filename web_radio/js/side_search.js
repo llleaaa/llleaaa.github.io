4079352a048182fe680e0cbf02904c0774145fc4
lea <chiiiiiu22@gmail.com>
2019-11-16 04:07:43 +0800

    commit

--- web_radio/js/side_search.js
+++ web_radio/js/side_search.js
@@ -16,13 +16,14 @@ $(document).ready(function(){
         //exeute the request
         request.execute(function(response){
             var results = response.result;
+            console.log(results);
             $.each(results.items, function(index, item){
                 var title = item.snippet.title.substring(0,40);
                 var videoid = item.id.videoId;
                 var desc = item.snippet.description.substring(0,40);
                 var thumb = item.snippet.thumbnails.medium.url;
                 $(".list").append(`
-                    <div class="side_article">
+                    <div class="side_article" data-key="${videoid}">
                         <iframe class="video w100" width="304" height="171" src="//www.youtube.com/embed/${videoid}" frameborder="0"></iframe>
                         <div class="side_details">
                             <h4>${title}</h4>
@@ -33,13 +34,24 @@ $(document).ready(function(){
                     </div>
                 `);  
                 
+                $('.list').on('click','.side_article', function(){
+                    var id = $(this).attr('data-key');
+                    mainVid(id);
+                });
             });
+            
             
         });
         
     });
     
 });
+
+function mainVid(id) {
+    $('.video-container').html(`
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/${id}?autoplay=1" frameborder="0" allow="accelerometer;  autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    `);
+} 
 
 
 function init(){
